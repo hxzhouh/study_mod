b728ee6fc1c89530eab3f06f319c8a55ef79b905
hxzhouh <hxzhouh@gmail.com>
2020-11-21 13:04:46 +0800

    A

--- basic/select/main.go
+++ basic/select/main.go
@@ -1,18 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync/atomic"
+)
+
+const (
+	mutexLocked      = 1 << iota // mutex is locked // 加锁状态
+	mutexWoken                   //是否有唤醒的goroutine
+	mutexStarving                // 从state字段中分出一个饥饿标记
+	mutexWaiterShift = iota      // 等待的 goroutines
+
+)
+
+type Mutex struct {
+	state uint32
+	sema  uint32
+}
 
 func main() {
-	c1 := make(chan bool, 1)
-	c2 := make(chan bool, 1)
-	for {
-		select {
-		case <-c1:
-		case <-c2:
-		default:
-			fmt.Print("hello")
 
-		}
+	fmt.Println(mutexLocked)
+	fmt.Println(mutexWoken)
+	fmt.Println(mutexStarving)
+	fmt.Println(mutexWaiterShift)
+	a := &Mutex{
+		state: 0,
+		sema:  0,
 	}
-
+	atomic.CompareAndSwapUint32(&a.state, 1, 0)
 }
