c7e9707572efe28afd7e0129e93460c64def9186
hxzhouh <hxzhouh@gmail.com>
2020-12-31 14:55:50 +0800

    144 迭代做法

--- leetcode/tree/144.go
+++ leetcode/tree/144.go
@@ -10,7 +10,7 @@ package tree
 144. 二叉树的前序遍历
 */
 //var result []int
-
+//https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
 func preorderTraversal(root *TreeNode) []int {
 	var vals []int
 	var helper114 func(node *TreeNode)
@@ -24,4 +24,26 @@ func preorderTraversal(root *TreeNode) []int {
 	}
 	helper114(root)
 	return vals
+}
+
+// 迭代 法
+func preorderTraversal1(root *TreeNode) []int {
+	for root == nil {
+		return nil
+	}
+	res := make([]int, 0)
+	queue := make([]*TreeNode, 0)
+	queue = append(queue, root)
+	for len(queue) > 0 {
+		head := queue[len(queue)-1]
+		queue = queue[:len(queue)-1]
+		for head != nil {
+			res = append(res, head.Val)
+			if head.Right != nil {
+				queue = append(queue, head.Right) // 右节点入栈，下一步处理左节点
+			}
+			head = head.Left
+		}
+	}
+	return res
 }
